b82042855a7c2273f9f5a69b399de80e5f905292
AiXueXiDeYu <2458002653@qq.com>
2023-01-09 21:33:26 +0800

    vue3 new composition + setup

---
+++ vue3/new_api/todos/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- hello world -->
+    <h1 @click="add" class="red">{{count}}<!--{{ state.count }}--></h1>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  // ref 响应式 简单数据
+
+  // let state = reactive({
+  //   count: 1
+  // })
+  let count = ref(1);
+  function add() {
+    count++;
+    // state.count++;
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
